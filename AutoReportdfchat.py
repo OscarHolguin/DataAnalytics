336d66981896dea41379ad8684a0b408fcc70503
OSCAR HOLGUIN <oscar.holguin@phi-1.com>
2024-02-05 08:59:34 -0700

    Added logo

--- AutoReportdfchat.py
+++ AutoReportdfchat.py
@@ -154,6 +154,16 @@ st.markdown(
 st.title(":bar_chart: :clipboard:")
 st.image('https://www.thinkdatadynamics.com/dark/assets/imgs/logo-light.png',width=350)
 
+
+st.set_page_config("Chat with data", page_icon="👋")
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+
 #st.image('https://static.wixstatic.com/media/d75272_496750661bda495c8beaea93cf6dfcab~mv2.png/v1/fill/w_230,h_60,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Asset%201Logo.png',width=400)
 
 st.subheader("AI Data Analyst")
